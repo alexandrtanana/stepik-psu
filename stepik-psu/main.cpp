32072f7cfbe037897f46a3742cde7fdcc2a45cd3
alexandrtanana <tanana1994@list.ru>
2022-11-20 19:58:42 +0300

    Массивы

--- stepik-psu/main.cpp
+++ stepik-psu/main.cpp
@@ -1,9 +1,30 @@
+/*Дан массив чисел. Выведите все элементы массива, которые больше предыдущего элемента .
+*/
+
+
+
 #include <iostream>
+#include <vector>
 using namespace std;
 int main() 
 {
-	setlocale(LC_ALL, "Rus");
-		int a;
-	cout << "Введите число: ";
-	cin >> a;
+        int n;
+        cin >> n;
+        vector <int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        for (int i = 0; i < n; i++) {
+            if ((arr[i] > 0) && (arr[i - 1] > 0) || (arr[i] < 0) && (arr[i - 1] < 0)) {
+                if (arr[i] > arr[i - 1]) {
+                    cout << arr[i - 1] << " " << arr[i];
+                }
+                else {
+                    cout << arr[i] << " " << arr[i - 1];
+                }
+                break;
+            }
+        }
+
+
 }
